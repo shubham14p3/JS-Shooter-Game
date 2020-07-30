13b14593b72e72efee6bd2499863d405e7384f3b
Shubham Raj <shubham14p3@gmail.com>
2020-07-30 17:28:26 +0530

    modified configuration

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,8 +1,21 @@
 import 'phaser';
 
 export default {
+  // eslint-disable-next-line no-undef
   type: Phaser.AUTO,
   parent: 'phaser-example',
-  width: 800,
-  height: 600
+  width: 1200,
+  height: 600,
+  backgroundColor: 'black',
+  dom: {
+    createContainer: true,
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
+  pixelArt: true,
+  roundPixels: true,
 };
