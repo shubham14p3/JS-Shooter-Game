d000b4b67a1880533e67264b36236e99b9e7244d
Shubham Raj <shubham14p3@gmail.com>
2020-07-30 17:29:59 +0530

    modified styling of the ui

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'phaser';
+import './stylesheet/style.css';
 import config from './Config/config';
 import GameScene from './Scenes/GameScene';
 import BootScene from './Scenes/BootScene';
@@ -6,10 +7,14 @@ import PreloaderScene from './Scenes/PreloaderScene';
 import TitleScene from './Scenes/TitleScene';
 import OptionsScene from './Scenes/OptionsScene';
 import CreditsScene from './Scenes/CreditsScene';
+import SceneGameOver from './Scenes/SceneGameOver';
+import LeaderBoardScene from './Scenes/LeaderBoardScene';
+import StoryScene from './Scenes/StoryScene';
 import Model from './Model';
 
+// eslint-disable-next-line no-undef
 class Game extends Phaser.Game {
-  constructor () {
+  constructor() {
     super(config);
     const model = new Model();
     this.globals = { model, bgMusic: null };
@@ -18,7 +23,10 @@ class Game extends Phaser.Game {
     this.scene.add('Title', TitleScene);
     this.scene.add('Options', OptionsScene);
     this.scene.add('Credits', CreditsScene);
+    this.scene.add('LeaderBoardScene', LeaderBoardScene);
     this.scene.add('Game', GameScene);
+    this.scene.add('SceneGameOver', SceneGameOver);
+    this.scene.add('StoryScene', StoryScene);
     this.scene.start('Boot');
   }
 }
