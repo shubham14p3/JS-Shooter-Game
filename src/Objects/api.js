5f0115c09468e4f3bf0e2c29b77c41b6639fb1ef
Shubham Raj <shubham14p3@gmail.com>
2020-07-30 17:31:33 +0530

    add api

---
+++ src/Objects/api.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-vars */
+const API = (() => {
+  async function getKey() {
+    try {
+      const response = await fetch(
+        "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: "DBZ Legend 2D Shooter",
+          }),
+        }
+      );
+      return response;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async function getScores() {
+    try {
+      const scores = await fetch(
+        "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ShHhUjBgHfAdsM/scores/",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      return scores.json();
+    } catch (error) {
+      return error.json();
+    }
+  }
+
+  async function postScores(name, score) {
+    try {
+      const result = await fetch(
+        "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ShHhUjBgHfAdsM/scores/",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            user: name,
+            score,
+          }),
+        }
+      );
+      return result.json();
+    } catch (error) {
+      return error.json();
+    }
+  }
+
+  return {getScores, postScores};
+})();
+
+export default API;
