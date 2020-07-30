e6cf68da26070aeb5685451e1944fba517dd164c
Shubham Raj <shubham14p3@gmail.com>
2020-07-30 17:37:34 +0530

    add server 8080 configuration

---
+++ server.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.set('port', process.env.PORT || 6000);
+
+var server = app.listen(app.get('port'), function () {
+  console.log('listening on port ', server.address().port);
+});
