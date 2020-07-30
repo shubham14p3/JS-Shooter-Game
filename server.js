e4288c15e89382def91d2b7616faedab68d19b66
Shubham Raj <shubham14p3@gmail.com>
2020-07-31 00:22:09 +0530

    initial commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.set('port', process.env.PORT || 6000);
+app.set('port', process.env.PORT || 8080);
 
 var server = app.listen(app.get('port'), function () {
   console.log('listening on port ', server.address().port);
