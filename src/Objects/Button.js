625539fca893f6fb1f0858e41fdbe4aaa25db64f
Shubham Raj <shubham14p3@gmail.com>
2020-07-30 17:31:50 +0530

    add button objects

--- src/Objects/Button.js
+++ src/Objects/Button.js
@@ -1,5 +1,6 @@
 import 'phaser';
 
+// eslint-disable-next-line no-undef
 export default class Button extends Phaser.GameObjects.Container {
   constructor(scene, x, y, key1, key2, text, targetScene) {
     super(scene);
@@ -8,23 +9,27 @@ export default class Button extends Phaser.GameObjects.Container {
     this.y = y;
 
     this.button = this.scene.add.sprite(0, 0, key1).setInteractive();
-    this.text = this.scene.add.text(0, 0, text, { fontSize: '32px', fill: '#fff' });
+    this.text = this.scene.add.text(0, 0, text, {
+      fontSize: '32px',
+      fill: '#fff',
+    });
+    // eslint-disable-next-line no-undef
     Phaser.Display.Align.In.Center(this.text, this.button);
 
     this.add(this.button);
     this.add(this.text);
 
-    this.button.on('pointerdown', function () {
+    this.button.on('pointerdown', () => {
       this.scene.scene.start(targetScene);
-    }.bind(this));
+    });
 
-    this.button.on('pointerover', function () {
+    this.button.on('pointerover', () => {
       this.button.setTexture(key2);
-    }.bind(this));
+    });
 
-    this.button.on('pointerout', function () {
+    this.button.on('pointerout', () => {
       this.button.setTexture(key1);
-    }.bind(this));
+    });
 
     this.scene.add.existing(this);
   }
