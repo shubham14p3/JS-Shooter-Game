682bdbc3e384c34c1d2a59698a048ee9f73e5ec9
Shubham Raj <shubham14p3@gmail.com>
2020-07-30 17:37:22 +0530

    add webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,28 @@
-'use strict';
-
-const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
+  entry: './src/index.js',
 
-    entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/build/',
+    filename: 'project.bundle.js',
+  },
 
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        publicPath: '/build/',
-        filename: 'project.bundle.js'
-    },
-
-    module: {
-        rules: [
-          {
-            test: [ /\.vert$/, /\.frag$/ ],
-            use: 'raw-loader'
-          }
-        ]
-    },
-
-    plugins: [
-        new webpack.DefinePlugin({
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
-    ]
-
+  module: {
+    rules: [
+      {
+        test: [/\.vert$/, /\.frag$/],
+        use: 'raw-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
 };
